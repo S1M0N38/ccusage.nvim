4ee1df5a96bbb5e0e08857871ca97b03b839dcc5
S1M0N38 <bertolottosimone@gmail.com>
2025-07-29 23:36:37 +0200

    feat: add init.lua for ccusage.nvim

---
+++ lua/ccusage/init.lua
@@ -0,0 +1,10 @@
+local M = {}
+
+---Setup the ccusage plugin
+---@param opts? CCUsage.UserOptions: plugin options
+---@return nil
+M.setup = function(opts)
+  require("ccusage.config").setup(opts)
+end
+
+return M
